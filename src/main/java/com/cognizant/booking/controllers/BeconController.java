a8a5e5c3235544e392c52ef1dd3b82cba563b508
Sumit Ray <sumit.ray@ehi.com>
2017-10-18 16:52:43 +0530

    Update BeconController.java

--- src/main/java/com/cognizant/booking/controllers/BeconController.java
+++ src/main/java/com/cognizant/booking/controllers/BeconController.java
@@ -3,6 +3,7 @@ package com.cognizant.booking.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cognizant.booking.dtos.DeviceInformation;
@@ -14,7 +15,7 @@ public class BeconController {
 	@Autowired
 	private BeconService beconService;
 
-	@RequestMapping("/api/booking/device/register")
+	@RequestMapping(value = "/api/booking/device/register", method = RequestMethod.POST)
 	public boolean registerBecons(@RequestBody final DeviceInformation deviceInformation) {
 		return beconService.registerBecon(deviceInformation);
 	}
