4c0879af68ac258c96ef19b0a0904cc979561241
Sumit Ray <sumit.ray@ehi.com>
2017-10-18 16:53:36 +0530

    Update ServiceTypes.java

--- src/main/java/com/cognizant/booking/enums/ServiceTypes.java
+++ src/main/java/com/cognizant/booking/enums/ServiceTypes.java
@@ -1,16 +1,23 @@
 package com.cognizant.booking.enums;
 
 public enum ServiceTypes {
-	POI("booking-engine-poi"), RESRVATION("booking-engine-reservation"), BECON("booking-engine-becon");
+	POI("booking-engine-poi", "info"), RESERVATION("booking-engine-reservation",
+			"info"), BECON_INFO("booking-engine-becon", "info"), BECON_REGISTER("booking-engine-becon", "register");
 
 	private String serviceType;
+	private String serviceCategory;
 
-	private ServiceTypes(String serviceType) {
+	private ServiceTypes(String serviceType, String serviceCategory) {
 		this.serviceType = serviceType;
+		this.serviceCategory = serviceCategory;
 	}
 
 	public String getServiceType() {
 		return serviceType;
+	}
+
+	public String getServiceCategory() {
+		return serviceCategory;
 	}
 
 }
