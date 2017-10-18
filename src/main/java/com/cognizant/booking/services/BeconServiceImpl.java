76dda47e32b5db775c4321eb54a3fc8f25389d89
Sumit Ray <sumit.ray@ehi.com>
2017-10-18 16:54:56 +0530

    Update BeconServiceImpl.java

--- src/main/java/com/cognizant/booking/services/BeconServiceImpl.java
+++ src/main/java/com/cognizant/booking/services/BeconServiceImpl.java
@@ -1,11 +1,13 @@
 package com.cognizant.booking.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.cognizant.booking.client.BookingApi;
 import com.cognizant.booking.dtos.DeviceInformation;
 import com.cognizant.booking.dtos.RegistrationInformation;
 
+@Service
 public class BeconServiceImpl implements BeconService {
 
 	@Autowired
