97526753872e11dbf2866efa62cb32acbe59ec07
Sumit Ray <sumit.ray@ehi.com>
2017-10-18 16:52:10 +0530

    Update BookingConfiguration.java

--- src/main/java/com/cognizant/booking/configs/BookingConfiguration.java
+++ src/main/java/com/cognizant/booking/configs/BookingConfiguration.java
@@ -1,8 +1,15 @@
 package com.cognizant.booking.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class BookingConfiguration {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 }
